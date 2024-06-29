1eeaf7677adaa35e69fe8fe014862318220d58a9
marilynakinyioyoo <marilyn.oyoo@student.moringaschool.com>
2024-06-29 18:51:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+const random = Math.ceil(Math.random() * 100 ) + 1
+const num3 = 49
+const num4 = 5
+const mod = num3 % num4
+let max = Math.max(1,2,3,4,6,20)
 
-// Write your code here
+
+
+
